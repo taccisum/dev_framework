17d45f4e840d4d8123191cfa1ec1d614eafb686d
taccisum <3277800095@qq.com>
2017-05-27 09:32:45 +0800

    1. 修改PaginationRequest字段与前端一致 2. PaginationRequest单元测试

--- fm-core/src/main/java/com/cloudlinkscm/loms/framework/core/pojo/PaginationRequest.java
+++ fm-core/src/main/java/com/cloudlinkscm/loms/framework/core/pojo/PaginationRequest.java
@@ -6,41 +6,78 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
  * @author : tac
  * @date : 2017/5/26
  */
-public class PaginationRequest {
-    private static final int DEF_PAGE_INDEX = 1;
+public class PaginationRequest<T> {
+    private static final int DEF_OFFSET = 0;
     private static final int DEF_PAGE_SIZE = 20;
-    private Integer pageIndex;
-    private Integer pageSize;
+    private Integer itemFrom;
+    private Integer itemTo;
 
-    public Integer getPageIndex() {
-        if(pageIndex == null || pageIndex <= 0){
-            return DEF_PAGE_INDEX;
+    @Deprecated
+    /**
+     * 由于该字段命名与实际意义不符，请避免在代码中直接使用该getter，使用 {@link #getOffset()} 方法代替
+     */
+    public Integer getItemFrom() {
+        if (itemFrom == null || itemFrom < 0) {
+            return DEF_OFFSET;
         }
-        return pageIndex;
+        return itemFrom;
     }
 
-    public void setPageIndex(Integer pageIndex) {
-        this.pageIndex = pageIndex;
+    @Deprecated
+    /**
+     * 由于该字段命名与实际意义不符，请避免在代码中直接使用该setter，使用 {@link #setOffset(Integer)}} 方法代替
+     */
+    public void setItemFrom(Integer itemFrom) {
+        this.itemFrom = itemFrom;
     }
 
-    public Integer getPageSize() {
-        if(pageSize == null || pageSize <= 0){
+    @Deprecated
+    /**
+     * 由于该字段命名与实际意义不符，请避免在代码中直接使用该getter，使用 {@link #getLimit()} 方法代替
+     */
+    public Integer getItemTo() {
+        if (itemTo == null || itemTo <= 0) {
             return DEF_PAGE_SIZE;
         }
-        return pageSize;
+        return itemTo;
     }
 
-    public void setPageSize(Integer pageSize) {
-        this.pageSize = pageSize;
+    @Deprecated
+    /**
+     * 由于该字段命名与实际意义不符，请避免在代码中直接使用该getter，使用 {@link #setLimit(Integer)} 方法代替
+     */
+    public void setItemTo(Integer itemTo) {
+        this.itemTo = itemTo;
     }
 
     @JsonIgnore
     public Integer getOffset() {
-        return (getPageIndex() - 1) * getPageSize();
+        return getItemFrom();
+    }
+
+    @JsonIgnore
+    public void setOffset(Integer offset) {
+        setItemFrom(offset);
     }
 
     @JsonIgnore
     public Integer getLimit() {
-        return getPageIndex() * getPageSize();
+        return getItemTo() - getItemFrom();
     }
+
+    @JsonIgnore
+    public void setLimit(Integer limit){
+        setItemTo(getOffset() + limit);
+    }
+
+    @JsonIgnore
+    public Integer getPageIndex(){
+        return getOffset() / getLimit() + 1;
+    }
+
+    @JsonIgnore
+    public Integer getPageSize(){
+        return getLimit();
+    }
+
 }
---
+++ fm-core/src/test/java/com/cloudlinkscm/loms/framework/core/pojo/PaginationRequestTest.java
@@ -0,0 +1,64 @@
+package com.cloudlinkscm.loms.framework.core.pojo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * todo::
+ * @author : tac
+ * @date : 2017/5/26
+ */
+public class PaginationRequestTest {
+
+    @Test
+    public void testDeprecated() {
+        PaginationRequest request = new PaginationRequest();
+        request.setItemFrom(0);
+        request.getItemFrom();
+    }
+
+    @Test
+    public void testGetOffset() {
+        PaginationRequest request = new PaginationRequest();
+        request.setOffset(12);
+        Assert.assertEquals(12, request.getOffset().intValue());
+        Assert.assertEquals(12, request.getItemFrom().intValue());
+    }
+
+    @Test
+    public void testGetLimit() {
+        PaginationRequest request = new PaginationRequest();
+        request.setOffset(12);
+        request.setLimit(3);
+
+        Assert.assertEquals(3, request.getLimit().intValue());
+        Assert.assertEquals(15, request.getItemTo().intValue());
+    }
+
+    @Test
+    public void testGetPageIndex() {
+        PaginationRequest request = new PaginationRequest();
+        request.setOffset(12);
+        request.setLimit(3);
+
+        Assert.assertEquals(12 / 3 + 1, request.getPageIndex().intValue());
+        Assert.assertEquals(3, request.getPageSize().intValue());
+    }
+
+    @Test
+    public void testSerializeDeserialize() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        PaginationRequest request = new PaginationRequest();
+        request.setOffset(12);
+        request.setLimit(3);
+
+        String serialize = mapper.writeValueAsString(request);
+        PaginationRequest deserialize = mapper.readValue(serialize, PaginationRequest.class);
+        Assert.assertEquals(12, deserialize.getItemFrom().intValue());
+        Assert.assertEquals(15, deserialize.getItemTo().intValue());
+    }
+
+}

33674c72d5781185cb607cfc2054d88d82a9b7a9
taccisum <3277800095@qq.com>
2017-05-27 18:48:14 +0800

    添加range()

--- fm-test/src/main/java/com/cloudlinkscm/loms/framework/test/GenericTest.java
+++ fm-test/src/main/java/com/cloudlinkscm/loms/framework/test/GenericTest.java
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author : tac
  * @date : 2017/5/27
@@ -25,4 +28,15 @@ public abstract class GenericTest {
     protected Logger logger = LoggerFactory.getLogger(this.getClass());
     protected Fairy fairy = Fairy.create();
 
+    protected List<Integer> range(int start, int end) {
+        List<Integer> range = new ArrayList<>();
+        for (int a = start; a < end; a++) {
+            range.add(a);
+        }
+        return range;
+    }
+
+    protected List<Integer> range(int count) {
+        return range(0, count);
+    }
 }

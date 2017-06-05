83be2c8c6c613025edafd320ab5bc5047f311de5
taccisum <3277800095@qq.com>
2017-06-05 16:44:31 +0800

    重复定义适配器时抛出RepeatAdapterException

---
+++ fm-webapp/src/main/java/com/cloudlinkscm/loms/framework/webapp/exception/RepeatAdapterException.java
@@ -0,0 +1,12 @@
+package com.cloudlinkscm.loms.framework.webapp.exception;
+
+/**
+ * @author : tac
+ * @date : 2017/6/5
+ */
+public class RepeatAdapterException extends Exception {
+
+    public RepeatAdapterException(String message) {
+        super(message);
+    }
+}
--- fm-webapp/src/main/java/com/cloudlinkscm/loms/framework/webapp/response/processer/adapter/ResponseAdapterFactory.java
+++ fm-webapp/src/main/java/com/cloudlinkscm/loms/framework/webapp/response/processer/adapter/ResponseAdapterFactory.java
@@ -1,9 +1,13 @@
 package com.cloudlinkscm.loms.framework.webapp.response.processer.adapter;
 
+import com.cloudlinkscm.loms.framework.webapp.exception.RepeatAdapterException;
+
 import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 响应适配器工厂，根据类别返回不同的{@link ResponseAdapter}实例
+ *
  * @author : tac
  * @date : 2017/5/17
  */
@@ -14,13 +18,14 @@ public class ResponseAdapterFactory {
     }
 
     /**
-     * 在实际应用中可通过此方法扩展需要的adapter
-     * @param type
-     * @param adapter
+     * 在实际应用中可通过此方法扩展自定义的adapter
+     *
+     * @param type 适配器类型，重复定义会抛出异常
+     * @param adapter 自定义的适配器
      */
-    public static void put(Integer type, ResponseAdapter adapter) throws Exception {
+    public static void put(Integer type, ResponseAdapter adapter) throws RepeatAdapterException {
         if (adapters.get(type) != null) {
-            throw new Exception("type: " + type + "is existed in collection");
+            throw new RepeatAdapterException("exist type: " + type);
         }
 
         adapters.put(type, adapter);
@@ -29,7 +34,7 @@ public class ResponseAdapterFactory {
     public static ResponseAdapter create(Integer type){
         ResponseAdapter adapter = adapters.get(type);
         if(adapter == null){
-            return new Response4ClientAdapter();
+            return adapters.get(0);
         }else{
             return adapter;
         }

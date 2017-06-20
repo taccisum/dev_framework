992646d8bc8aea43d49dc456e8af00d060f1bf4d
taccisum <3277800095@qq.com>
2017-06-20 17:09:22 +0800

    添加注释

--- fm-core/src/main/java/com/cloudlinkscm/loms/framework/core/constant/SwaggerExample.java
+++ fm-core/src/main/java/com/cloudlinkscm/loms/framework/core/constant/SwaggerExample.java
@@ -12,9 +12,9 @@ public interface SwaggerExample {
     /**
      * 参数为字典值
      */
-    String DICTIONARY = "{dictionary}";
+    String DICTIONARY = "{dictionary_key}";
     /**
      * 参数为特定日期格式字符串
      */
-    String DATE = "{DATE | yyyy-MM-dd hh:mm:ss}";
+    String DATE = "2017-06-20 00:00:00";
 }

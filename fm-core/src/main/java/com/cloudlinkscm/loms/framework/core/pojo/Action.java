e7cebd7a0ae05ea2cf74f9e58432606083d99280
taccisum <3277800095@qq.com>
2017-06-22 15:43:42 +0800

    无参无返回值的函数接口

---
+++ fm-core/src/main/java/com/cloudlinkscm/loms/framework/core/pojo/Action.java
@@ -0,0 +1,13 @@
+package com.cloudlinkscm.loms.framework.core.pojo;
+
+/**
+ * 定义一个无参无返回值的函数接口
+ *
+ * @author : tac
+ * @date : 2017/6/22
+ */
+
+@FunctionalInterface
+public interface Action {
+    void doAction();
+}

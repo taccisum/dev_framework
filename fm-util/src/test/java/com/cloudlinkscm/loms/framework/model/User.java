3a894f4f0831ecee504792ab6dc56753eb606790
taccisum <3277800095@qq.com>
2017-06-22 12:05:10 +0800

    定义常量

--- fm-util/src/test/java/com/cloudlinkscm/loms/framework/model/User.java
+++
@@ -1,7 +0,0 @@
-package com.cloudlinkscm.loms.framework.model;
-
-/**
- * Created by DELL on 2017/6/2.
- */
-public class User {
-}
--- fm-util/src/test/java/com/cloudlinkscm/loms/framework/util/ExcelUtilsTest.java
+++
@@ -1,7 +0,0 @@
-package com.cloudlinkscm.loms.framework.util;
-
-/**
- * Created by DELL on 2017/6/2.
- */
-public class ExcelUtilsTest {
-}

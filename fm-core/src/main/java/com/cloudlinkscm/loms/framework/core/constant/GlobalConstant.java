d861a23692c8647388afd5e86c3d53fe592a0270
taccisum <3277800095@qq.com>
2017-06-29 10:46:31 +0800

    全局常量

---
+++ fm-core/src/main/java/com/cloudlinkscm/loms/framework/core/constant/GlobalConstant.java
@@ -0,0 +1,10 @@
+package com.cloudlinkscm.loms.framework.core.constant;
+
+/**
+ * @author : tac
+ * @date : 2017/6/23
+ */
+
+public interface GlobalConstant {
+    String DATE_TIME_FORMAT_PATTERN = "yyyy-MM-dd HH:mm:ss";
+}
